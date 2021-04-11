ecea532acaca14194b9bef220b93507b2a19f2cc
Manish976 <shakyamanish978@gmail.com>
2021-04-11 15:15:50 +0545

    deleted all non used variable from khaja order full

--- src/App.js
+++ src/App.js
@@ -18,24 +18,24 @@ import "./theme.scss";
 import fakeBackend from './helpers/AuthType/fakeBackend';
 
 //Firebase helper
-import { initFirebaseBackend } from "./helpers/firebase_helper";
+// import { initFirebaseBackend } from "./helpers/firebase_helper";
 
-// Activating fake backend
-fakeBackend();
+// // Activating fake backend
+// fakeBackend();
 
-const firebaseConfig = {
-	apiKey: process.env.REACT_APP_APIKEY,
-	authDomain: process.env.REACT_APP_AUTHDOMAIN,
-	databaseURL: process.env.REACT_APP_DATABASEURL,
-	projectId: process.env.REACT_APP_PROJECTID,
-	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-	appId: process.env.REACT_APP_APPID,
-	measurementId: process.env.REACT_APP_MEASUREMENTID,
-};
+// const firebaseConfig = {
+// 	apiKey: process.env.REACT_APP_APIKEY,
+// 	authDomain: process.env.REACT_APP_AUTHDOMAIN,
+// 	databaseURL: process.env.REACT_APP_DATABASEURL,
+// 	projectId: process.env.REACT_APP_PROJECTID,
+// 	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+// 	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+// 	appId: process.env.REACT_APP_APPID,
+// 	measurementId: process.env.REACT_APP_MEASUREMENTID,
+// };
   
-// init firebase backend
-initFirebaseBackend(firebaseConfig);
+// // init firebase backend
+// initFirebaseBackend(firebaseConfig);
 
 class App extends Component {
 	constructor(props) {
